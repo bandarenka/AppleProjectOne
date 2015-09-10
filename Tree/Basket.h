f39bc82e83f6a002378a26ecc19a26ebf563bf2c
Admin <admin@Mac-Admin.local>
2015-09-10 19:03:13 +0300

    Add Basket class

---
+++ Tree/Basket.h
@@ -0,0 +1,13 @@
+//
+//  Basket.h
+//  Tree
+//
+//  Created by Admin on 10.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Basket : NSObject
+
+@end
---
+++ Tree/Basket.m
@@ -0,0 +1,15 @@
+//
+//  Basket.m
+//  Tree
+//
+//  Created by Admin on 10.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import "Basket.h"
+
+@implementation Basket
+
+
+
+@end
