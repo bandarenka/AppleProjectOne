53188d0f12a525fb71bc1c1e3d07cf5e68d48239
Admin <admin@Mac-Admin.local>
2015-09-17 13:30:43 +0300

    Fail to set Unit tests

---
+++ TreeTests.h
@@ -0,0 +1,13 @@
+//
+//  TreeTests.h
+//  Tree
+//
+//  Created by Admin on 17.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+@interface TreeTests : XCTestCase
+
+@end
---
+++ TreeTests.m
@@ -0,0 +1,13 @@
+//
+//  TreeTests.m
+//  Tree
+//
+//  Created by Admin on 17.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import "TreeTests.h"
+
+@implementation TreeTests
+
+@end
